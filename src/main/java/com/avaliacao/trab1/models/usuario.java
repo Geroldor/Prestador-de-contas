abd04dd924ce4a552441f1f834aa1c3acf376253
Geroldor <geraldo.antonio@uel.br>
2025-11-23 20:45:03 -0300

    adicionado recursos de conta

---
+++ src/main/java/com/avaliacao/trab1/models/usuario.java
@@ -0,0 +1,31 @@
+package com.avaliacao.trab1.models;
+
+public class usuario {
+    private int idUsuario;
+    private String nick;
+    private String senha;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
